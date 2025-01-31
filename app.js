a1bc680ba5f52c51ee0232fa3d0c97b92d59a3e7
Rafael Rodrigues <rodriguesjesus704@gmail.com>
2025-01-31 19:16:04 -0300

    verificnado se a lista esta vazia e adicionando nomes a lista

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = []
+
+
+function validarEntradaInput() {
+    let amigo = document.querySelector("input").value;
+    if (amigo == ""){
+        alert("Digite um nome por favor");
+    }else{
+        listaAmigos.push(amigo);
+        limparCampo();
+        
+    }
+}
+   
+function adicionarAmigo(){
+    validarEntradaInput();
+    console.log(listaAmigos)
+       
+}
+
+function limparCampo(){
+    limpar = document.querySelector("input");
+    limpar.value = "";
+}
+
+
+
+
+adicionarAmigo();
